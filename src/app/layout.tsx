5da06576f5a7275961316e5905cdef098e26a7c1
marcioeugenio <129126182+marcioeugenio@users.noreply.github.com>
2025-06-10 02:01:56 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-br">
+      <head />
       <body>{children}</body>
     </html>
   );
